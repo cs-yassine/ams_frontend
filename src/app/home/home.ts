796e18715250c0f85e725e365cb9184e6784415c
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-10-01 12:23:49 +0100

    property binding et event binduing

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -10,4 +10,9 @@ export class Home {
  university:string="Sesame";
  names:string[]=["yassine","faten","ayoub","fares","jihen","ghassen","aziz","amine","sabrine","farah"]
  nbrEtudiants :number=26;
+ srcLogo:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGakkUEbXyfann4M16v9CV-sTa915cUOdh9g&s";
+ display():void
+ {
+  alert("Hello World");
+ }
 }
