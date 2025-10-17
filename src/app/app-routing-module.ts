6108510ac9cb3cef756d2f79cfcccebfe479c428
Yassine Khefifi <yassinekhefifi.student@gmail.com>
2025-10-17 16:12:17 +0100

    navigation entre les composants listprovider et add provider

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Contact } from './contact/contact';
+import { ListProviders } from './list-providers/list-providers';
+import { AddProvider } from './add-provider/add-provider';
 
 const routes: Routes = [
+  {path:"listProviders",
+    component:ListProviders
+  },
+  {
+    path:"addProvider",
+    component:AddProvider
+  },
   {
     path:"home",
     component:Home
